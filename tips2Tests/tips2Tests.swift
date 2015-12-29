17fe304e1136a7e8d396e5be9015f5c3b7b05cde
H Ji <hji@hs-macbook-pro.local>
2015-12-29 11:32:46 -0600

    Initial Commit

---
+++ tips2Tests/tips2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  tips2Tests.swift
+//  tips2Tests
+//
+//  Created by H Ji on 12/29/15.
+//  Copyright © 2015 H Ji. All rights reserved.
+//
+
+import XCTest
+@testable import tips2
+
+class tips2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
